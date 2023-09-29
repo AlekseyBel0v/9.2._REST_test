8b7c9862173f8ad5bbe7dc5f6e4cc8507b47eab5
Aleksei <aleksej@MacBook-Pro-Aleksej.local>
2023-09-29 20:26:14 +0300

    addBranchOfConfigurationVia@Value

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,23 +1,17 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-/*
-Через аннотацию @ConfigurationProperties задается свойство hello. в Файле application.properties через это свойство и точку
-идет обращение к переменной from и ей присваивается значение. Для работы Спринга необходимо, чтобы у него был доступ к переменной
-через сетер.
- */
-@ConfigurationProperties("hello")
+
 @RestController
 public class HelloController {          //т.к. класс этот класс находится в директории главного класса, то аннотация @SpringBootApplication
+    @Value("${hello.from:undefined}")
     private String from;                //подтянется автоматически и будет создан и зарегистрирован новый бин
 
-    public void setFrom(String from) {
-        this.from = from;
-    }
+//    public void setFrom(String from) {        //сетер не нужен, т.к. поле будет задано через Рефлекшн апи
+//        this.from = from;
+//    }
 
     @GetMapping("/")
     private String hello() {
