67d21fa640d25f4f3dff6e63932dd8592a34be11
Aleksei <aleksej@MacBook-Pro-Aleksej.local>
2023-10-03 15:40:43 +0300

    add_person_date_in_GET_request

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,23 +2,23 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Person;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class HelloController {          //т.к. класс этот класс находится в директории главного класса, то аннотация @SpringBootApplication
+public class HelloController {
     @Value("${hello.from:undefined}")
-    private String from;                //подтянется автоматически и будет создан и зарегистрирован новый бин
+    private String from;
 
-//    public void setFrom(String from) {        //сетер не нужен, т.к. поле будет задано через Рефлекшн апи
-//        this.from = from;
-//    }
-
-    @PostMapping("/hello")
-    private String hello(@RequestBody Person guest) {
+    @GetMapping("/hello")
+    private String hello(@RequestParam Person guest) {
         return String.format("Hello from %s, name %s age %d",
                 from, guest.getName(), guest.getAge());
     }
+
+//    Пример десерреализации параметров из тела постЗапроса в объект класса
+//    @PostMapping("/hello")
+//    private String hello(@RequestBody Person guest) {
+//        return String.format("Hello from %s, name %s age %d",
+//                from, guest.getName(), guest.getAge());
+//    }
 }
